d8f0875172ee71548621a994420eabad9ed3626e
Amit Kashyap <mrkashyapamit@gamil.com>
2023-09-26 21:02:20 +0530

    First

---
+++ amitKashyap.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        //Write your code here
+        Scanner scanner = new Scanner(System.in);
+        int b = scanner.nextInt();
+        int a = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        System.out.println(b);
+        System.out.println(a);
+        System.out.println(c);
+
+    }
+}
